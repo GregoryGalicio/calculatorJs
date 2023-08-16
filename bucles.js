248cd746d98b737c6d244327679b5ee10605cb64
GregoryGalicio <gregory.galicios@ciplima.org.pe>
2023-08-15 23:06:02 -0500

    finish

--- bucles.js
+++ bucles.js
@@ -48,3 +48,27 @@ const sumatoria = () => {
 }
 let resultado=sumatoria()
 console.log(resultado)
+
+console.log("-------------EJERCICIO 3) FACTORIAL--------------")
+
+const mostrarFactorial = (x) => {
+    
+    if(x>0){
+
+        let factorial=1
+        for (let i = 1; i <= x; i++) {
+            //bloque
+            // console.log(`este es el numero ${i} la suma momentanea es ${sumador+i}`)
+            factorial=factorial*i
+        }
+        return factorial
+    }
+    else{
+        console.log("EL numero es negativo no tiene factorial")
+    }
+    
+
+
+}
+let resultadoFactorial=mostrarFactorial(-2)
+console.log(resultadoFactorial)
